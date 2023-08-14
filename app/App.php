918bb9f503edfbb84d1e18f53114f10029a289d6
Andrew Surya <andrewsuryaaccforprogramming@gmail.com>
2023-08-14 20:07:46 +0800

    all data stored in an array

--- app/App.php
+++ app/App.php
@@ -2,4 +2,28 @@
 
 declare(strict_types = 1);
 
+$content = [];
 // Your Code
+
+function storeCSVFile(string $csvFilePath)
+{
+    global $content; // cause function is local scope like domain expansion in jujustu kaisen
+
+    $handle = fopen($csvFilePath, 'r');
+
+    if ($handle !== false)
+    {
+        while(($line = fgetcsv($handle)) !== false) 
+        {
+            array_push($content, $line);
+            
+        }
+        
+    }
+    else
+    {
+        echo 'File not found';
+        return false;
+    }
+
+}
--- public/index.php
+++ public/index.php
@@ -9,3 +9,22 @@ define('FILES_PATH', $root . 'transaction_files' . DIRECTORY_SEPARATOR);
 define('VIEWS_PATH', $root . 'views' . DIRECTORY_SEPARATOR);
 
 /* YOUR CODE (Instructions in README.md) */
+
+// include necessary file 
+require APP_PATH . "App.php";
+require VIEWS_PATH . "transactions.php";
+
+if (!file_exists(FILES_PATH . "sample_1.csv"))
+{
+    echo "File not Found";
+    return;
+}
+else
+{   
+    $file_path = FILES_PATH . "sample_1.csv";
+}
+
+
+// read csv file
+storeCSVFile($file_path); // all ada stored in $content array of array
+
